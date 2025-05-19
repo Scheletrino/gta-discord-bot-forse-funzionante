55ac6f8a77494e948c8bddae8d236616f242114b
Scheletrino <andreafec10@gmail.com>
2025-05-19 18:28:36 +0200

    Update minigioco.js

--- commands/minigioco.js
+++ commands/minigioco.js
@@ -3,9 +3,15 @@ const { SlashCommandBuilder } = require('discord.js');
 const questions = require('../data/questions.json');
 const activeQuestions = {};
 
+const CANALE_AUTORIZZATO = '1372183510500114543';
+
 module.exports = {
   data: new SlashCommandBuilder().setName('minigioco').setDescription('Inizia un quiz GTA!'),
   async execute(interaction) {
+    if (interaction.channel.id !== CANALE_AUTORIZZATO) {
+      return interaction.reply({ content: '❌ Questo comando può essere usato solo nel canale designato.', ephemeral: true });
+    }
+
     const random = questions[Math.floor(Math.random() * questions.length)];
     const formatted = random.options.join('\n');
     activeQuestions[interaction.user.username] = random.correct;
