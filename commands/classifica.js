183524197504ce2ef540b0cb1e24d984f71e38b5
Scheletrino <andreafec10@gmail.com>
2025-05-19 18:28:06 +0200

    Update classifica.js

--- commands/classifica.js
+++ commands/classifica.js
@@ -1,10 +1,15 @@
-
 const { SlashCommandBuilder } = require('discord.js');
 const { getLeaderboard } = require('../utils/scoreManager');
+
+const CANALE_AUTORIZZATO = '1372183510500114543';
 
 module.exports = {
   data: new SlashCommandBuilder().setName('classifica').setDescription('Mostra la classifica del quiz'),
   async execute(interaction) {
+    if (interaction.channel.id !== CANALE_AUTORIZZATO) {
+      return interaction.reply({ content: '❌ Questo comando può essere usato solo nel canale designato.', ephemeral: true });
+    }
+
     const leaderboard = getLeaderboard();
     if (leaderboard.length === 0) {
       return interaction.reply('📉 Nessun punteggio registrato ancora!');
@@ -14,3 +19,4 @@ module.exports = {
     return interaction.reply(`🏆 **Classifica GTA Quiz**\n\n${lines.join('\n')}`);
   }
 };
+
